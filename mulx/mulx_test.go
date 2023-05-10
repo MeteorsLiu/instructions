ed8946cf011973cc244cc78e754673747c82e316
GuoHaoLan <coopticks@hotmail.com>
2023-05-10 18:55:39 +0800

    add benchmark

--- mulx/mulx_test.go
+++ mulx/mulx_test.go
@@ -1,7 +1,37 @@
 package mulx
 
-import "testing"
+import (
+	"math/bits"
+	"testing"
+)
 
 func TestMulx(t *testing.T) {
 	t.Log(Mulx(5, 6))
+}
+
+func BenchmarkMulxSmall(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Mulx(5, 6)
+	}
+}
+
+func BenchmarkGoSmall(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bits.Mul64(5, 6)
+	}
+}
+func BenchmarkMulxLarge(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Mulx(0xa0761d6478bd642f, 0xe7037ed1a0b428db)
+	}
+}
+
+func BenchmarkGoLarge(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bits.Mul64(0xa0761d6478bd642f, 0xe7037ed1a0b428db)
+	}
 }
