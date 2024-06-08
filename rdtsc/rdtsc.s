018942c4e1286510b89d8330a1d640faca0b78ed
unknown <coopticks@outlook.com>
2024-06-08 15:44:29 +0800

    add nanotime compare

--- rdtsc/rdtsc.s
+++ rdtsc/rdtsc.s
@@ -2,9 +2,7 @@
 
 // func rdtsc() (n uint64)
 TEXT ·rdtsc(SB),NOSPLIT,$0
-    BYTE	$0x0F // RDTSCP
-	BYTE	$0x01
-	BYTE	$0xF9
+    RDTSCP
     SHLQ  $32, DX
     ADDQ  DX, AX
     MOVQ  AX, n+0(FP)
--- rdtsc/rdtsc_test.go
+++ rdtsc/rdtsc_test.go
@@ -1,7 +1,43 @@
 package rdtsc
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	_ "unsafe"
+)
+
+//go:linkname nanotime runtime.nanotime
+func nanotime() int64
 
 func TestRdtsc(t *testing.T) {
-	t.Log(RDTSCP())
+	t1 := RDTSCP()
+	//t.Log(t1)
+	time.Sleep(time.Second)
+	t2 := RDTSCP()
+
+	t.Log(time.Duration(t2 - t1).Seconds())
+}
+
+func TestNanotime(t *testing.T) {
+	t1 := nanotime()
+	//t.Log(t1)
+	time.Sleep(time.Second)
+	t2 := nanotime()
+
+	t.Log(time.Duration(t2 - t1).Seconds())
+}
+
+func BenchmarkRDTSCP(b *testing.B) {
+	b.Run("rdtscp", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			RDTSCP()
+		}
+	})
+
+	b.Run("nanotime", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			nanotime()
+		}
+	})
 }
